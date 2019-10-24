3d58a21a1bd789607b358b86f58075d36aa98f81
Yoko1127 <56930185+Yoko1127@users.noreply.github.com>
2019-10-24 12:02:33 +0900

    修正

--- src/dcnet/DCNET.java
+++ src/dcnet/DCNET.java
@@ -16,6 +16,7 @@ public class DCNET {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("woaichixuegao");
     }
     
 }
